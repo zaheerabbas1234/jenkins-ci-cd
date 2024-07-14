2c1a3714eeda5fd05819889efe5971f0fc4e3929
Sk. M. Zaheer Abbas <zaheer.abbas1506@gmail.com>
2024-07-14 11:40:14 +0530

    second commit

--- src/main/java/com/jenkins/controller/JenkinsController.java
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -2,9 +2,12 @@ package com.jenkins.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jenkins.model.Jenkins;
 import com.jenkins.repository.JenkinsRepository;
 
 
@@ -18,6 +21,14 @@ public class JenkinsController {
 	@GetMapping("/test")
 	public String test() {
 		return "welcome to jenkins ci-cd";
+	}
+	
+	@PostMapping("/add")
+	public Jenkins add(@RequestBody Jenkins model) {
+		
+		Jenkins save = repo.save(model);
+		
+		return save;
 	}
 
 }
